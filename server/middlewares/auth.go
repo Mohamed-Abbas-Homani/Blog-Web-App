de02ea8312d836a50e1c5937bc5b894d33da04d3
Mohamed-Abbas-Homani <mohamedabbashoumani@gmail.com>
2024-02-16 19:49:24 +0200

    Auth middleware

--- server/middlewares/auth.go
+++ server/middlewares/auth.go
@@ -1,2 +1,53 @@
 package middlewares
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	jwt "github.com/golang-jwt/jwt/v5"
+)
+
+func Auth(c *gin.Context) {
+	tokenString :=  c.GetHeader("Authorization")
+	var body struct {
+		id uint
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to read body"})
+		return
+	}
+
+	if tokenString == "" {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	token, err := validateJWT(tokenString)
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	if !token.Valid {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	if body.id != claims["userID"] {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	c.Next()
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("JWT_SECRET")
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(secret), nil
+	})
+}
