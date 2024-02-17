4f4d788478187f4df77da9ea9d90dfb8f510a1fc
Mohamed-Abbas-Homani <mohamedabbashoumani@gmail.com>
2024-02-17 16:05:13 +0200

    ...

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,15 +7,16 @@ import { PersistGate } from "redux-persist/integration/react";
 import {persistStore} from "redux-persist"
 import store from './services/store.js';
 import { RouterProvider } from 'react-router-dom';
-import router from './services/router.js';
+import router from './services/router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        <RouterProvider router={router} />
+    {/* <Provider store={store}> */}
+      {/* <PersistGate loading={null} persistor={persistStore(store)}> */}
+        {/* <RouterProvider router={router} /> */}
+        <p>adsfadsfadsf</p>
         <App />
-      </PersistGate>
-    </Provider>
+      {/* </PersistGate> */}
+    {/* </Provider> */}
   </React.StrictMode>,
 )
--- client/src/services/router.jsx
+++ client/src/services/router.jsx
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import NotFoundPage from "../pages/NotFoundPage";
+import ProfilePage from "../pages/ProfilePage";
 
 const router = createBrowserRouter([
   {
